4d1e2162d8e239a66d9b562c09059b0bf874e913
Илья Соколов <sokolovila543@gmail.com>
2024-05-21 00:52:20 +0300

    encode to cc3d well

--- label_connected_components.py
+++ label_connected_components.py
@@ -5,7 +5,8 @@ import open3d as o3d
 from classes.PCD import PCD
 import matplotlib.pyplot as plt
 import cc3d #connected-components
-
+from PIL import Image
+import PIL
 
 def load_pcd(file_path):
     pc_data = PCD()
@@ -42,6 +43,43 @@ def label_connected_components_cc3d(voxel_indices, connectivity, delta):
     return labels_out , N
 
 
+def encode_voxels_to_cc3d_format(voxel_indices):
+    '''
+    voxel_indices.shape = (x, 3)  
+    '''
+# пример
+#     filled = np.array([       #y0        #y1      ...
+#                             [[0, 0, 1],[1, 0, 1],[1, 0, 1],[0, 0, 1],[0, 0, 1],[0, 0, 1]], #x0
+#                             [[1, 0, 0],[1, 0, 0],[1, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]], #x1
+#                             [[1, 0, 0],[1, 0, 0],[1, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]],
+#                             [[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 1, 0],[0, 1, 0],[0, 1, 0]],
+#                             [[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 1, 0],[0, 1, 0],[0, 1, 0]],
+#                             [[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 1, 0],[0, 1, 0],[0, 1, 0]]   
+#                             # а внутри координаты, сколько брать вверх по z
+# ])
+
+    
+    x_max = np.max(voxel_indices[:, 0])
+    y_max = np.max(voxel_indices[:, 1])
+    z_max = np.max(voxel_indices[:, 2])
+    
+    labels = np.zeros((x_max+1,y_max+1,z_max+1)) 
+
+    for x,y,z in voxel_indices:
+        labels[x,y,z] = 1
+       
+    # print(labels.shape)
+    # print(labels)
+    # ax = plt.figure().add_subplot(projection='3d')
+    # ax.voxels(labels,  edgecolor='k')
+
+    # plt.show()
+    return labels
+   
+
+
+
+
 def map_voxel_labels_to_points(points, voxel_indices, labeled_array, voxel_size):
     voxel_indices_dict = {tuple(idx): label for idx, label in zip(voxel_indices, labeled_array[tuple(voxel_indices.T)])}
     labels = []
@@ -57,6 +95,7 @@ def create_labeled_point_cloud(voxel_indices, labeled_array):
     pcd.points = o3d.utility.Vector3dVector(voxel_indices)
     pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
     return pcd, labels
+
 
 def save_labeled_pcd(original_pc_data, labels, output_path):
     # Ensure labels are of the correct dtype (uint32)
@@ -85,8 +124,10 @@ def save_labeled_pcd(original_pc_data, labels, output_path):
     new_pc_data.save(output_path, verbose=True)
 
 
+
+
 if __name__ == "__main__":
-    voxel_size = 0.05
+    voxel_size = 0.5
     connectivity = 26
     input_path = "trees.pcd"  # "/path/to/input.las", or "/path/to/input.pcd"
     output_path = "trees_voxeled.pcd"  # "/path/to/output.pcd" !only *.pcd!
@@ -101,12 +142,19 @@ if __name__ == "__main__":
     voxel_indices = voxel_grid_to_numpy(voxel_grid)
     
     
-   
+    # print(voxel_indices)
     
-    labels_out,_ = label_connected_components_cc3d(voxel_indices, connectivity, delta = 10)
+    cc3d_labels = encode_voxels_to_cc3d_format(voxel_indices)
+    
+    
+    labels_out,N_components = label_connected_components_cc3d(cc3d_labels, connectivity = connectivity, delta = 0)
 
-    print(labels_out)
    
+    print(labels_out[0])
+
+    print(labels_out.shape)
+    print('Number of components detected:',N_components)
+
     # labels = map_voxel_labels_to_points(points, voxel_indices, labeled_array, voxel_size)
 
 
@@ -121,11 +169,11 @@ if __name__ == "__main__":
 
     
     
-    # pcd = o3d.geometry.PointCloud()
+    pcd = o3d.geometry.PointCloud()
     
-    # pcd.points = o3d.utility.Vector3dVector(labels_out)
+    pcd.points = o3d.utility.Vector3dVector(labels_out)
     # # pcd.colors = o3d.utility.Vector3dVector(np.ones(voxel_indices.shape)*np.array([123,233,40]) /255)
     
-    # o3d.visualization.draw_geometries([pcd])
+    o3d.visualization.draw_geometries([pcd])
     
     
